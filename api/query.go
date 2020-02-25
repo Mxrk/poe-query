78310dae3ff37693a6e63008ed8893768cdcfa3d
Mark <mxrk@users.noreply.github.com>
2020-02-25 01:32:01 +0100

    fix

--- api/query.go
+++ api/query.go
@@ -1,6 +1,11 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
 func Api(w http.ResponseWriter, r *http.Request) {
 
@@ -19,4 +24,26 @@ func Api(w http.ResponseWriter, r *http.Request) {
 	s := parse(vars[0])
 	w.Write([]byte(s))
 
+}
+
+func parse(url string) string {
+
+	resp, err := http.Get("https://www.pathofexile.com/trade/search/Metamorph/" + url)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	msg := "{\"query\":{\"status\":{\"option\":\"online\"},"
+	contents, err := ioutil.ReadAll(resp.Body)
+	// s := strings.Split(string(contents), " require([\"main\"]")
+	s2 := strings.Split(string(contents), "\"state\":")
+
+	s3 := strings.Split(s2[1], "//-->")
+	s4 := strings.Split(s3[0], ",\"loggedIn\"")
+	msg += s4[0][1:]
+	msg += ",\"sort\":{\"price\":\"asc\"}}"
+
+	msg = strings.Replace(msg, ",\"status\":\"online\"", "", -1)
+	return msg
 }
